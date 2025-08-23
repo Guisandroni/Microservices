e5453f5d2283abaa97909bcc9beba29a60f6c675
guisandroni <gui.sandroni@gmail.com>
2025-08-23 13:07:08 -0300

    fix: dependency swagger spring doc with OPENAPI

--- store/src/main/java/dev/guisandroni/store/Config/OpenAPISwagger.java
+++ store/src/main/java/dev/guisandroni/store/Config/OpenAPISwagger.java
@@ -1,5 +1,7 @@
 package dev.guisandroni.store.Config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,10 @@ public class OpenAPISwagger {
 
     @Bean
     OpenAPI customOpenAPI(){
-
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Api Store Microservice")
+                        .version("1.0")
+                        .description("Documentação API"));
     }
 }
