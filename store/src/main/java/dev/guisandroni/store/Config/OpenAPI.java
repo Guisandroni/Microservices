54a5d1a4b3ece2d270e19a4a8a537dfcf82a159d
guisandroni <gui.sandroni@gmail.com>
2025-08-23 12:35:08 -0300

    fix: rename class config

---
+++ store/src/main/java/dev/guisandroni/store/Config/OpenAPI.java
@@ -0,0 +1,4 @@
+package dev.guisandroni.store.Config;
+
+public class OpenAPI {
+}
--- store/src/main/java/dev/guisandroni/store/Config/Rabbit.java
+++ store/src/main/java/dev/guisandroni/store/Config/Rabbit.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Config {
+public class Rabbit {
     @Bean
     Jackson2JsonMessageConverter jsonMessageConverter (){
         return new Jackson2JsonMessageConverter();
