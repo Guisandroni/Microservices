9c12663c9b2a63e87b0f0ed1c13a6876e261a2a9
guisandroni <gui.sandroni@gmail.com>
2025-08-23 12:39:02 -0300

    fix: add dependency swagger core

--- store/src/main/java/dev/guisandroni/store/Config/OpenAPI.java
+++
@@ -1,4 +0,0 @@
-package dev.guisandroni.store.Config;
-
-public class OpenAPI {
-}
---
+++ store/src/main/java/dev/guisandroni/store/Config/OpenAPISwagger.java
@@ -0,0 +1,15 @@
+package dev.guisandroni.store.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class OpenAPISwagger {
+
+    @Bean
+    OpenAPI customOpenAPI(){
+
+    }
+}
